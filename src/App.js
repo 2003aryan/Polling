b82da1ef02e9c5dc692012d5a694083d29cf312b
Aryan Garg <aryangarg@Aryan-Mac-M1.local>
2023-06-06 18:44:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import CreatePoll from './components/create_poll.jsx';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <CreatePoll/>
+    </div>
+  );
+}
+
+export default App;

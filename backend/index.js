be1a94dfd790ea5a2567623a1ede92014df08c6b
Aryan Garg <aryan.garg.21cse@bmu.edu.in>
2023-06-16 20:44:47 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,27 @@
-// To connect with your mongoDB database
 const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nirnaymittal:myself5430@cluster0.gzoaymz.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+const port = 5001;
+const uri = 'mongodb+srv://nirnaymittal:myself5430@cluster0.gzoaymz.mongodb.net/?retryWrites=true&w=majority';
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("cluster0").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connectToMongoDB() {
+	try {
+		await client.connect();
+		await client.db().command({ ping: 1 });
+		console.log('Pinged your deployment. You successfully connected to MongoDB!');
+	}
+	catch (error) {
+		console.error('Failed to connect to MongoDB:', error);
+	}
 }
-run().catch(console.dir);
 
-// Schema for users of app
+connectToMongoDB();
+
 const UserSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -43,41 +37,29 @@ const UserSchema = new mongoose.Schema({
 		default: Date.now,
 	},
 });
+
 const User = mongoose.model('Users', UserSchema);
 
-// For backend and express
-const express = require('express');
-const app = express();
-const cors = require("cors");
-console.log("App listen at port 5000");
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, resp) => {
 
-	resp.send("App is Working");
-	client.db("nirnay").collection('Users').insertOne({'name' :"Nirnay" , 'email' :'nirnay@test.com'})
-	// You can check backend is working or not by
-	// entering http://loacalhost:5000
-	
-	// If you see App is working means
-	// backend working properly
+app.get('/', (req, resp) => {
+	resp.send('App is working');
 });
 
-app.post("/register", async (req, resp) => {
+app.post('/register', async (req, resp) => {
 	try {
 		const user = new User(req.body);
-		let result = await user.save();
-		result = result.toObject();
-		if (result) {
-			delete result.password;
-			resp.send(req.body);
-			console.log(result);
-		} else {
-			console.log("User already register");
-		}
-
-	} catch (e) {
-		resp.send("Something Went Wrong");
+		const result = await user.save();
+		const { password, ...userData } = result.toObject();
+		resp.send(userData);
+		console.log(userData);
+	} catch (error) {
+		console.error('Failed to register user:', error);
+		resp.status(500).send('Something went wrong');
 	}
 });
-app.listen(5000);
+
+app.listen(port, () => {
+	console.log(`App listening at http://localhost:${port}`);
+});

e240aa9734fcb2fbcb977b1b77d3e8e951283652
Nirnay Mittal <nirnaymittal06@gmail.com>
2023-06-16 16:09:55 +0530

    Mongodb setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
 // To connect with your mongoDB database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://aryangarg:aryangarg@aryan.bej9llo.mongodb.net/?retryWrites=true&w=majority', {
-	dbName: 'Aryan',
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-}, err => err ? console.log(err) :
-	console.log('Connected to your database'));
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nirnaymittal:myself5430@cluster0.gzoaymz.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("cluster0").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Schema for users of app
 const UserSchema = new mongoose.Schema({
@@ -23,8 +43,7 @@ const UserSchema = new mongoose.Schema({
 		default: Date.now,
 	},
 });
-const User = mongoose.model('users', UserSchema);
-User.createIndexes();
+const User = mongoose.model('Users', UserSchema);
 
 // For backend and express
 const express = require('express');
@@ -36,6 +55,7 @@ app.use(cors());
 app.get("/", (req, resp) => {
 
 	resp.send("App is Working");
+	client.db("nirnay").collection('Users').insertOne({'name' :"Nirnay" , 'email' :'nirnay@test.com'})
 	// You can check backend is working or not by
 	// entering http://loacalhost:5000
 	
